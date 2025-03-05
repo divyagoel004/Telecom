ff31582ea70693b9d911aaf5bd21eec05aeac49c
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2025-03-05 10:10:10 +0530

    Update hn.py

--- hn.py
+++ hn.py
@@ -271,11 +271,11 @@ def show_card_details(selected_card):
         m=repr(m)
         with col1:
             st.write("### Data Distribution")
-            fig = px.histogram(filtered_data, x=filtered_data.m)
+            fig = px.histogram(filtered_data, x=filtered_data[m])
             st.plotly_chart(fig, use_container_width=True, height=300)
         with col2:
             st.write("### Trend Over Time")
-            time_fig = px.line(filtered_data, x="recorded_at", y=filtered_data.m)
+            time_fig = px.line(filtered_data, x="recorded_at", y=filtered_data[m])
             st.plotly_chart(time_fig, use_container_width=True, height=300)
         csv = filtered_data.to_csv(index=False)
         st.download_button(label="📥 Download Filtered Data", data=csv, file_name=f"{selected_card['type']}_data.csv", mime="text/csv")
