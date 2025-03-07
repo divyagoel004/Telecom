4e47b4db5cd1a6680e27a2e5961464e7995e982a
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2025-03-07 14:40:07 +0530

    Update hn.py

--- hn.py
+++ hn.py
@@ -69,7 +69,7 @@ def transform_data(force=True):
 
     # Generate 100 random timestamps within the last 24 hours.
     now = datetime.now()
-    start_time = now - timedelta(hours=30)
+    start_time = now - timedelta(hours=72)
     start_ts = int(start_time.timestamp())
     end_ts = int(now.timestamp())
     random_ts = np.random.randint(start_ts, end_ts, 100)
