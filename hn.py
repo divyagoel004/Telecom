ef00304ed089f6303879e793ed5783dd5f10b6a3
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2025-04-08 10:35:31 +0530

    Update hn.py

--- hn.py
+++ hn.py
@@ -30,7 +30,7 @@ api = "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"
 try:
     from groq import Groq
     from together import Together
-    groq_client = Client(api_key=api)
+    groq_client = Groq(api_key=api)
     os.environ['TOGETHER_API_KEY'] = "a566a4825abfe3bf9a18dc844ea8275bf9496105475b3a7211862ccc51a22316"
     together_client = Together(api_key=os.environ["TOGETHER_API_KEY"])
 except ImportError:
